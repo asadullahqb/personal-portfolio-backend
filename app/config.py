98d775eaf8dd3fd4e5b10bbe780f5eefd4b634cb
asadullahqb <asadullah.q.bhatti@gmail.com>
2025-11-10 12:02:55 +0800

    Remove the mlflow

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    mlflow_tracking_uri: str = "file:///tmp/mlruns"
     country_to_language: str = "t5-small"
     translator_model_name: str = "Helsinki-NLP/opus-mt-en-mul"
 
